597c400c5d96fd686ee251eceb63369301cbfc65
myssh <69339069+sady2009-wq@users.noreply.github.com>
2021-09-14 20:04:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,4 +15,5 @@ sed -i 's/10.10.10.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/$1$ttZeRDG3$V0pEG8rex5imt2uWJp3gO\/:18875:0:99999:7:::/g' package/base-files/files/etc/shadow
 wget https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.106.3/AdGuardHome_linux_arm64.tar.gz
 tar zxvf AdGuardHome_linux_arm64.tar.gz
-cp -a AdGuardHome/AdGuardHome files
+mkdir files
+mv ./AdGuardHome/AdGuardHome .files/
