6a58de27bd50adb1379d37063733d1894846e057
myssh <69339069+sady2009-wq@users.noreply.github.com>
2021-09-30 22:15:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,10 +14,10 @@
 sed -i 's/10.10.10.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/$1$ttZeRDG3$V0pEG8rex5imt2uWJp3gO\/:18875:0:99999:7:::/g' package/base-files/files/etc/shadow
 #Add Adguardhome bin file
-wget https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.106.3/AdGuardHome_linux_arm64.tar.gz
-tar zxvf AdGuardHome_linux_arm64.tar.gz
-mkdir -p files/usr/bin
-cp -a ./AdGuardHome/AdGuardHome files/usr/bin/
+#wget https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.106.3/AdGuardHome_linux_arm64.tar.gz
+#tar zxvf AdGuardHome_linux_arm64.tar.gz
+#mkdir -p files/usr/bin
+#cp -a ./AdGuardHome/AdGuardHome files/usr/bin/
 # modify wifi ssid 
 sed -i 's/OpenWrt_2.4G/OpenWrt_5G/g'  package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '185s/OpenWrt_5G/OpenWrt_2.4G/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
